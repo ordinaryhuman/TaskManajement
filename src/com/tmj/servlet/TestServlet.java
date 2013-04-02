a1cfd3a867eeefb5e14311e7b3aefe32fbd1adea
Abdurrosyid Handoyo <arrosyidbh@gmail.com>
2013-04-02 20:35:25 +0700

    add TestServlet

--- src/com/tmj/servlet/TestServlet.java
+++ src/com/tmj/servlet/TestServlet.java
@@ -1,10 +1,27 @@
 package com.tmj.servlet;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 public class TestServlet extends BaseController {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -6907668313636219837L;
+	
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		super.doGet(request, response);
+		
+		mRD = request.getRequestDispatcher("test.jsp");
+		mRD.forward(request, response);
+	}
 
 }
